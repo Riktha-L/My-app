2f9265df5d2fbb6c8330b26218be967128f864d0
Riktha-L <Riktha.l2022ai-ml@sece.ac.in>
2025-04-23 11:42:04 +0530

    Exercise 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to React</h1>
+      <p>React is a JavaScript library for building user interfaces</p> 
+      <p>It is maintained by Facebook and a community of individual developers and companies</p>
     </div>
-  );
+  ); 
 }
-
 export default App;
